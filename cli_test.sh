6428d32251a55ed315d46a28a71fd91ee4026422
myfreess <neomyfreess@outlook.com>
2026-04-11 22:56:32 +0800

    update

--- cli_test.sh
+++ cli_test.sh
@@ -3,7 +3,7 @@ set -euox pipefail
 
 token_diff() {
   name=$1
-  ./target/native/release/build/tokendiff/cli/cli.exe "cli_test/source/${name}.old.mbt" "cli_test/source/${name}.new.mbt" >"cli_test/snapshot/${name}.txt"
+  ./_build/native/release/build/tokendiff/cli/cli.exe "cli_test/source/${name}.old.mbt" "cli_test/source/${name}.new.mbt" >"cli_test/snapshot/${name}.txt"
 }
 
 token_diff "20251114"
